ad93719a80f264569f640ce00a2196cbc69303a5
Kevin Marrec <kevin@marrec.io>
2025-08-28 23:34:13 +0200

    feat: v1 (#2)

--- index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: '@antfu',
-  rules: {
-    '@typescript-eslint/no-unused-vars': ['warn'],
-    '@typescript-eslint/space-before-function-paren': ['error', 'always']
-  }
-}
